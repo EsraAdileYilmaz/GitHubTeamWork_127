d62837c0cb738403966c789773856d5f511afea4
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2023-12-14 09:43:20 +0100

    first commit

---
+++ src/OrtakClass.java
@@ -0,0 +1,21 @@
+public class OrtakClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Burada bir babayigit gecti");
+
+
+
+
+
+
+        /*
+        git init
+       git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/EsraAdileYilmaz/GitHubTeamWork_127.git
+git push -u origin main
+         */
+    }
+}
---
+++ src/babayigit.java
@@ -0,0 +1,6 @@
+public class babayigit {
+    public static void main(String[] args) {
+
+        System.out.println("bbayigit hoca");
+    }
+}
